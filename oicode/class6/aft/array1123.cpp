980d00a5d765fa85d133b214c7ce08a070344c8c
Alice Margatroid <kino3z@yuyuko.cc>
2017-07-29 00:48:26 +0800

    Add files via upload

---
+++ oicode/class6/aft/array1123.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {
+    int count=0,m,n;
+    cin>>m>>n;
+    int j1[m][n];
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            cin>>j1[i][j];
+    for (int i = 0; i < m; ++i)
+        for (int m,j = 0; j < n; ++j)
+        {cin>>m;if(m==j1[i][j]) count+=1;}
+    printf("%.2lf",(double(count)/(m*n))*100);
+    return 0;
+}
---
+++ oicode/class6/aft/array1124.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int a[n][m],b[n][m];
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            cin >> a[i][j];
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            cin >> b[i][j];
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < m; ++i) {
+            cout<<a[k][i]+b[k][i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
