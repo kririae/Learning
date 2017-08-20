6ad3552a0e74d3f62f5e3f7b8a67563e432aafef
kririae <kririae@yuyuko.cc>
2017-08-20 10:08:43 -0500

    2017-8-20 update

---
+++ oicode/class8/function1413.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+struct accurate{
+    int value[1000];
+    int length;
+    accurate(){
+        memset(value,-1,sizeof(value));length = 0;
+    }
+};
+accurate convert(int num,int binary){
+    accurate res,placed;
+    int i = 0;
+    while(num>0){
+        res.value[i] = num%binary;
+        num/=binary;++i;
+    }
+    for(int i = 0;i < 100;++i){
+        if(res.value[i]==-1)break;
+        else res.length+=1;
+    }
+    placed = res;
+    for(int i = 0;i < res.length; ++i){
+        res.value[i] = placed.value[res.length-i-1];
+    }
+    return res;
+}
+bool count_a_b(accurate x){
+    int res=0;
+    for (int i = 0; i < x.length; ++i)
+        res+=x.value[i];
+    if(res>x.length-res) return true;
+    else return false;
+}
+int main() {
+    int a,b;
+    a = b = 0;
+    for (int i = 1; i <= 1000; ++i) {
+        if(count_a_b(convert(i,2))) ++a;
+        else ++b;
+    }
+    cout<<a<<" "<<b;
+    return 0;
+}
---
+++ oicode/class8/recursive1158.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int sum(int x){
+    if(x == 1) return 1;
+    return x + sum(x-1);
+}
+int main() {
+    int res;
+    cin>>res;
+    cout<<sum(res);
+    return 0;
+}
