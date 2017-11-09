89e3bddbdf443ddcc2f4d4035d455748a247ad53
kriaeth <kriaeth@outlook.com>
2017-11-09 23:51:54 +0800

    today

---
+++ oicode/template/dp/采药.cpp
@@ -0,0 +1,10 @@
+/*
+* by kriaeth 2017/11/8/20:44
+* 采药
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	// 不做了
+    return 0;
+}
--- oicode/ybt/CLASS26_athome/1306.cpp
+++ oicode/ybt/CLASS26_athome/1306.cpp
@@ -1,49 +1,92 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
-#include <cstdio>
+/*
+* by kriaeth 2017/11/8 17:32
+* lis
+*/
+#include <bits/stdc++.h>
 using namespace std;
 struct posi {
 	int v;
 	int a;
 	int b;
 };
+int v[505][505] = {0};
+int a[505] = {0};
+int b[505] = {0};
+int memo[505][505] = {0};
+posi value[505];
 vector<int> res;
-posi v[20005];
-int main() {
-	freopen("test.in", "r", stdin);
-	freopen("output2.txt", "w", stdout);
-	int n;
-	cin >> n;
-	for(int i = 1; i <= n; ++i) {
-		cin >> v[i].v;
-		v[i].a = 1;
-		v[i].b = 0;
+vector<int> res2;
+void initlcs() {
+	int length = res.size();
+	for(int i = 0; i < length; ++i) {
+		value[i + 1].v = res[i];
+		value[i + 1].a = 1;
+		value[i + 1].b = 0;
 	}
-	for(int i = 1; i <= n; ++i) {
+	for(int i = 1; i <= length; ++i) {
 		int maxn = 0;
 		for(int j = i - 1; j >= 1; --j) {
-			if(v[i].v >= v[j].v && v[j].a > maxn) {
-				maxn = v[j].a;
-				v[i].b = j;
+			if(value[i].v > value[j].v && value[j].a > maxn) {
+				maxn = value[j].a;
+				value[i].b = j;
 			}
 		}
-		v[i].a = maxn + 1;
+		value[i].a = maxn + 1;
 	}
 	int result = 0;
 	int max_index = 0;
-	for(int i = 1; i <= n; ++i) 
-		if(v[i].a > result) {
-			result = v[i].a;
+	for(int i = 1; i <= length; ++i) 
+		if(value[i].a > result) {
+			result = value[i].a;
 			max_index = i;
 		}
-	cout << "maxn=" << result << endl;
+	cout << result << endl;
 	while(max_index >= 1) {
-		res.push_back(v[max_index].v);
-		max_index = v[max_index].b;
+		res2.push_back(value[max_index].v);
+		max_index = value[max_index].b;
 	}
-	for(int i = res.size() - 1; i >= 0; --i) 
-		cout << res[i] << " ";
-	fclose(stdin);
-	return 0;
+	for(int i = res2.size() - 1; i >= 0; --i) 
+		cout << res2[i] << " ";
+}
+void initres(int x, int y) {
+	if(x == 0 || y == 0) return;
+	if(memo[x][y] == 1) {
+		initres(x - 1, y - 1);
+		res.push_back(a[x]);
+	}
+	else {
+		if(memo[x][y] == 0) initres(x, y - 1);
+		else if(memo[x][y] == -1) initres(x - 1, y);
+	}
+}
+int main() {
+	int n, m;
+	cin >> n;
+	for(int i = 1; i <= n; ++i) 
+		cin >> a[i];
+	cin >> m;
+	for(int i = 1; i <= m; ++i) 
+		cin >> b[i];
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= m; ++j) {
+            if(a[i] == b[j]) {
+                v[i][j] = v[i - 1][j - 1] + 1;
+                memo[i][j] = 1;
+            }
+            else {
+                v[i][j] = max(v[i - 1][j], v[i][j - 1]);
+                if(v[i - 1][j] < v[i][j - 1]) {
+                	v[i][j] = v[i][j - 1];
+                	memo[i][j] = 0;
+                }
+                else {
+                	v[i][j] = v[i - 1][j];
+                	memo[i][j] = -1;
+                }
+            }
+        }
+    }
+    initres(n, m);
+	initlcs();
+    return 0;
 }
