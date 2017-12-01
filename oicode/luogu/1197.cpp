b28a22b776140c4a9245006698d1e204c088da54
kririae <kririae@yuyuko.cc>
2017-12-02 02:00:12 +0800

    galgame

--- oicode/luogu/1197.cpp
+++ oicode/luogu/1197.cpp
@@ -3,47 +3,46 @@ using namespace std;
 int n, m, k, father[200005];
 vector<int> result;
 struct edge {
-	int x, y;
+  int x, y;
 } p[200005];
 inline int find(int val) {
-	if(father[val] != val) father[val] = find(val);
-	return father[val];
+  if (father[val] != val)
+    father[val] = find(val);
+  return father[val];
 }
-inline void unionn(int val1, int val2) {
-	father[find(val1)] = find(val2);
-}
+inline void unionn(int val1, int val2) { father[find(val1)] = find(val2); }
 int main() {
-	cin.tie(0);
-	ios::sync_with_stdio(false);
-	cin >> n >> m;
-	for (int i = 0; i <= n; ++i) 
-		father[i] = i;
-	for (register int i = 0; i < m; ++i) 
-		cin >> p[i].x >> p[i].y;
-	cin >> k;
-	for (int val, i = 0; i < k; ++i) {
-		cin >> val;
-		for(int i = 0; i < m; ++i) {
-			if(p[i].x == val || p[i].y == val) {
-				unionn(p[i].x, p[i].y);
-			}
-		}
-		// 查找联通数量
-		for (int i = 0; i <= n; ++i) 
-			int tempo = find(i);
-		// 化为两层的树形结构
-		int count = 1;
-		int flag = father[0];
-		for (int i = 1; i <= n; ++i) {
-			if(father[i] != flag) {
-				flag = father[i];
-				++count;
-			}
-		}
-		result.push_back(count);
-	}
-	for(int i = result.size() - 1; i >= 0; --i) {
-		cout << result[i] << endl;
-	}
-	return 0;
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cin >> n >> m;
+  for (int i = 0; i <= n; ++i)
+    father[i] = i;
+  for (register int i = 0; i < m; ++i)
+    cin >> p[i].x >> p[i].y;
+  cin >> k;
+  for (int val, i = 0; i < k; ++i) {
+    cin >> val;
+    for (int i = 0; i < m; ++i) {
+      if (p[i].x == val || p[i].y == val) {
+        unionn(p[i].x, p[i].y);
+      }
+    }
+    // 查找联通数量
+    for (int i = 0; i <= n; ++i)
+      int tempo = find(i);
+    // 化为两层的树形结构
+    int count = 1;
+    int flag = father[0];
+    for (int i = 1; i <= n; ++i) {
+      if (father[i] != flag) {
+        flag = father[i];
+        ++count;
+      }
+    }
+    result.push_back(count);
+  }
+  for (int i = result.size() - 1; i >= 0; --i) {
+    cout << result[i] << endl;
+  }
+  return 0;
 }
--- oicode/template/graph/1382.cpp
+++ oicode/template/graph/1382.cpp
@@ -4,39 +4,37 @@ using namespace std;
 const ll maxn = 100005;
 typedef long long ll;
 struct edge {
-    ll to;
-    ll value;
-    edge(ll a, ll b) {
-        to = a, value = b;
-    }
-}
-vector<edge> graph[maxn];
+  ll to;
+  ll value;
+  edge(ll a, ll b) { to = a, value = b; }
+} vector<edge> graph[maxn];
 queue<ll> proc;
 ll dist[maxn];
 void add_edge(ll from, ll to, ll value) {
-    graph[from].push_back(edge(to, value));
+  graph[from].push_back(edge(to, value));
 }
 void SPFA() {
-    proc.push(1);
-    dist[1] = 0;
-    while(!proc.empty()) {
-        ll current = proc.front();
-        proc.pop();
-        for(int i = 0; i < graph[current].size(); ++i) {
-            if(dist[graph[current][i].to] > dist[current] + graph[current][i].value) {
-                dist[graph[current][i].to] = dist[current] + graph[current][i].value;
-                proc.push(graph[current][i].to);
-            }
-        }
+  proc.push(1);
+  dist[1] = 0;
+  while (!proc.empty()) {
+    ll current = proc.front();
+    proc.pop();
+    for (int i = 0; i < graph[current].size(); ++i) {
+      if (dist[graph[current][i].to] >
+          dist[current] + graph[current][i].value) {
+        dist[graph[current][i].to] = dist[current] + graph[current][i].value;
+        proc.push(graph[current][i].to);
+      }
     }
+  }
 }
 ll main() {
-    ll n, m;
-    cin >> n >> m;
-    memset(dist, LLONG_MAX, sizeof(dist));
-    for(ll ai, bi, ci, i = 1; i <= n; ++i) {
-        scanf("%d%d%d", &ai, &bi, &ci);
-        add_edge(ai, bi, ci);
-    }
-    cout << dist[n] << endl;
+  ll n, m;
+  cin >> n >> m;
+  memset(dist, LLONG_MAX, sizeof(dist));
+  for (ll ai, bi, ci, i = 1; i <= n; ++i) {
+    scanf("%d%d%d", &ai, &bi, &ci);
+    add_edge(ai, bi, ci);
+  }
+  cout << dist[n] << endl;
 }
