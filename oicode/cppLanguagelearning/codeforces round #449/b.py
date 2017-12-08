605fd0bd70efff088b824f2fb71ad0f118f45e6d
kriaeth <kriaeth@yuyuko.cc>
2017-12-08 22:01:14 +0800

    update

--- oicode/cppLanguagelearning/codeforces round #449/b.py
+++
@@ -1,9 +0,0 @@
-a, b = [int(i) for i in str(input()).split(" ")]
-count = 0
-summ = 0
-for i in range(1, a + 1):
-    tempo = ''.join([i for i in list(str(i))])
-    tempo += ''.join(list(reversed(tempo)))
-    summ += int(str(tempo))
-    summ %= b
-print(summ)
