bd6da823d85b5e6715316d82bad77b16e6308f28
kririae <kririae@yuyuko.cc>
2017-12-29 22:07:32 +0800

    bst

--- oicode/template/tree/平衡树/bst_basic.cpp
+++ oicode/template/tree/平衡树/bst_basic.cpp
@@ -1,44 +1,32 @@
+#pragma GCC optimize(2)
 #include <bits/stdc++.h>
-
 using namespace std;
 namespace BST {
     const int maxn = 1e5 + 5;
-
     struct node {
-        int val;
-        int lson, rson;
-        int father;
-        int size;
-        int rtag; // remove tag
+        int val, lson, rson, father, size, rtag; // remove tag
         node() : val(0), lson(0), rson(0), father(0), size(0) {}
     };
-
     node t[maxn];
     vector<int> templ;
     int cnt = 1, root = 1;
-
     inline void print(int k, bool typ) {
         if (t[k].lson) print(t[k].lson, typ);
         if (!typ) cout << t[k].val << " ";
         else templ.push_back(t[k].val);
         if (t[k].rson) print(t[k].rson, typ);
     }
-
     inline void pushup(int k) {
         t[k].size = t[t[k].lson].size + t[t[k].rson].size + 1;
     }
-
     inline void insert(int k, int val) {
         // root -> k = 0
-        // ??????????bst??...
         if (cnt == 1) {
-            t[cnt++].val = val;
-            t[cnt].size = 1;
-            t[cnt].father = 0;
-            pushup(k);
-            return;
+            t[cnt++].val = val; t[cnt].size = 1; t[cnt].father = 0;
+            pushup(k); return;
         }
         node &curr = t[k];
+        bool uselson = val <= curr.val;
         if (val <= curr.val) {
             if (curr.lson == 0) {
                 t[cnt].val = val;
@@ -58,19 +46,6 @@ namespace BST {
         }
         pushup(k);
     }
-
-    /*
-    inline void del(int k, int val) {
-        if(k == 0) return;
-        if(t[k].val == val) {
-            if(t[k].rson != 0) {
-
-            }
-        }
-        if(val <= t[k].val) del(t[k].lson, val);
-        else del(t[k].rson, val);
-    }
-    */
     inline void subreb(int &k, int l, int r) {
         if (l > r) return;
         int mid = (l + r) >> 1;
@@ -79,7 +54,6 @@ namespace BST {
         subreb(t[k].lson, l, mid - 1);
         subreb(t[k].rson, mid + 1, r);
         t[t[k].lson].father = t[t[k].rson].father = k; // TODO
-        // t[k].size = t[ t[k].lson ].size + t[ t[k].rson ].size + 1;
         pushup(k);
     }
 
@@ -88,14 +62,16 @@ namespace BST {
         print(k, true); // ????????????????lst(???)
         subreb(k, 0, templ.size() - 1);
     }
-
-    inline int getrank(int k, int val) {
-        if (k == 0) return;
-        if (t[k].size == val) return t[k].val;
-        else {
-            if (t[k].size > val) getrank(t[k].lson, val);
-            else getrank(t[k].rson, val - t[k].size - 1);
-        }
+    inline int find(int k, int val) {
+        if(t[k].val == val) return k;
+        if(val <= t[k].val && t[k].lson) return find(t[k].lson, val);
+        else if(t[k].rson) return find(t[k].rson, val);
+    }
+    inline int getkth(int k, int val) {
+        // by LittleRewriter
+        if(t[t[k].lson].size + 1 == val) return t[k].val;
+        else if(t[k].lson && val <= t[t[k].lson].size) return getkth(t[k].lson, val);
+        else if(t[k].rson && val >= t[t[k].lson].size + 2) return getkth(t[k].rson, val - t[t[k].lson].size - 1);
     }
 }
 
@@ -107,25 +83,13 @@ int main() {
         cin >> k;
         insert(root, k);
     }
-    /*
     for (int i = 1; i < cnt; ++i) {
         cout << t[t[i].father].val << " " <<t[i].size << " " << t[i].val << endl;
     }
-    */
-    cout << getrank(root, 3) << endl;
+    // cout << getrank(root, 3) << endl;
     print(root, false);
-    /*
-    int temp = cnt;
-    print(root, false);
-    for (int i = root; i < n; ++i) {
-        rebuild(i);
-        for (int i = root; i <= cnt; ++i)
-            cout << t[i].val << " ";
-        cout << endl;
-    }
-    print(temp + 1, false);
-    // cout << endl << cnt << endl;
-    */
+    cout << endl;
+    cout << getkth(root, 3) << endl;
 }
 /*
 5
