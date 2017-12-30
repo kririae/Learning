4efb82b0a015639adbf706b081c55cea61f36807
kriaeth <kriaeth@outlook.com>
2017-12-31 00:53:22 +0800

    e17

--- oicode/contests/codeforces/goodBye2017/c.cpp
+++ oicode/contests/codeforces/goodBye2017/c.cpp
@@ -2,10 +2,12 @@
 // Created by AliceMargatroid on 2017/12/30.
 //
 #include <bits/stdc++.h>
+
 using namespace std;
 double n, r;
 vector<double> pos;
 double res[10005];
+
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
@@ -18,7 +20,7 @@ int main() {
     res[0] = r;
     for (int i = 0; i < pos.size(); ++i) {
         for (int j = 0; j < i; ++j) {
-            double delx =  fabs(pos[i] - pos[j]);
+            double delx = fabs(pos[i] - pos[j]);
             res[i] = max(res[i], res[j] + sqrt(4 * r * r - delx * delx));
         }
     }
