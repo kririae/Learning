0fa4aa1cb5350dfb3cdc177461b629f559077a08
kririae <kririae@yuyuko.cc>
2017-12-30 14:56:30 +0800

    调好了！！！我的rotate调好了！！！

--- oicode/template/tree/bst_basic.cpp
+++ oicode/template/tree/bst_basic.cpp
@@ -9,7 +9,7 @@ namespace BST {
     }; 
     struct node {
         treap messt;
-        int val, father, size, rtag, son[2]; // remove tag
+        int val, father, size, turn, son[2]; // remove tag
         node() : val(0), father(0), size(0) {}
     };
     node t[maxn];
@@ -28,16 +28,19 @@ namespace BST {
     inline void pushup(int k) {
         t[k].size = t[t[k].son[0]].size + t[t[k].son[1]].size + 1;
     }
+    inline void pushdown(int k) {
+        if(t[k].size == 0);
+    }
     inline void insert(int k, int val) {
         // root -> k = 0
         if (cnt == 1) {
-            t[cnt++].val = val; t[cnt].size = 1; t[cnt].father = 0;
+            t[cnt++].val = val; t[cnt].size = 1; t[cnt].father = 0; t[cnt].turn = 0;
             pushup(k); return;
         }
         node &curr = t[k];
         int proc = val > curr.val;
         if(curr.son[proc] == 0) {
-            t[cnt].val = val, curr.son[proc] = cnt, t[cnt].father = k, t[cnt].size = 1;
+            t[cnt].val = val, curr.son[proc] = cnt, t[cnt].father = k, t[cnt].size = 1, t[cnt].turn = 0;
             ++cnt;
         } else insert(curr.son[proc], val);
         pushup(k);
@@ -49,7 +52,7 @@ namespace BST {
         t[k].val = templ[mid];
         subreb(t[k].son[0], l, mid - 1);
         subreb(t[k].son[1], mid + 1, r);
-        t[t[k].son[0]].father = t[t[k].son[1]].father = k; // è‡ªå·±å„¿å­çš„çˆ¶äº²æ˜¯è‡ªå·±(é‡ç½®)
+        t[t[k].son[0]].father = t[t[k].son[1]].father = k; // ×Ô¼º¶ù×ÓµÄ¸¸Ç×ÊÇ×Ô¼º(ÖØÖÃ)
         pushup(k);
     }
     inline void rebuild(int k) {
@@ -79,7 +82,7 @@ namespace BST {
         else if(t[k].son[0] && t[k].val > val) return subgetrank(t[k].son[0], val, cnt);
         else if(t[k].son[1] && t[k].val < val) return subgetrank(t[k].son[1], val, cnt + t[t[k].son[0]].size + 1);
     }
-    // å› ä¸ºç‰¹åˆ¤çš„åŽŸå›  ä¸èƒ½ç›´æŽ¥è°ƒç”¨ æ‰€ä»¥åŠ äº†ä¸€ä¸ªå­å‡½æ•°
+    // ÒòÎªÌØÅÐµÄÔ­Òò ²»ÄÜÖ±½Óµ÷ÓÃ ËùÒÔ¼ÓÁËÒ»¸ö×Óº¯Êý
     inline int getrank(int k, int val) {
         if(!find(k, val)) return 0;
         return subgetrank(k, val, 0);
@@ -87,44 +90,76 @@ namespace BST {
     /*
     inline void rotate(int k) {
         // meth == 0 ? meth = 1 : meth = 0;
-        // meth == 0 å³æ—‹ else å·¦æ—‹
+        // meth == 0 ÓÒÐý else ×óÐý
         int sonindex = t[k].son[0], currindex = k, fatherindex = t[k].father;
         node &son = t[sonindex], &curr = t[k], &father = t[fatherindex];
-        // å…³äºŽfatherçš„æ“ä½œ
+        // ¹ØÓÚfatherµÄ²Ù×÷
         if(k == father.son[0]) father.son[0] = sonindex;
         if(k == father.son[1]) father.son[1] = sonindex;
-        // å…³äºŽå½“å‰èŠ‚ç‚¹çš„æ“ä½œ
+        // ¹ØÓÚµ±Ç°½ÚµãµÄ²Ù×÷
         curr.son[0] = son.son[1];
         curr.father = currindex;
-        // å…³äºŽå­èŠ‚ç‚¹çš„æ“ä½œ
+        // ¹ØÓÚ×Ó½ÚµãµÄ²Ù×÷
         son.father = fatherindex;
         son.son[1] = currindex;
-        // pushupæ“ä½œ
+        // pushup²Ù×÷
         pushup(k); 
         pushup(sonindex);
         pushup(fatherindex);
-    }*/
-    inline void rotate(int k, int meth) {
+    }
+    inline int rotate(int k, int meth) {
         int curr = k;
         int fa = t[k].father;
         int fafa = t[fa].father;
         node &currn = t[k], &fan = t[fa], &fafan = t[fafa];
-        for (int i = 0; i < 2; ++i) if(fan.son[i] && meth == i ^ 1) return; // é”™è¯¯å¤„ç†
+        for (int i = 0; i < 2; ++i) if(fan.son[i] && meth == i ^ 1) return -1; // ´íÎó´¦Àí
         fan.son[meth] = currn.son[meth ^ 1], t[currn.son[meth ^ 1]].father = fa;
         currn.son[meth ^ 1] = fa, fan.father = curr, currn.father = fafa;
         if(fafan.son[meth] == fa) fafan.son[meth] = curr;
         if(fafan.son[meth ^ 1] == fa) fafan.son[meth ^ 1] = curr;
         pushup(fa); pushup(curr); pushup(fafa);
+        return 0; // Õý³£½áÊø
+    }*/
+    inline void rotate(int k) {
+        // if father node of k is root, would there need to be a spec?
+        int curr = k;
+        int fa = t[k].father;
+        int fafa = t[fa].father;
+        bool ifrotateff = true;
+        // if(fafa == 0) return; 
+        if(fafa == 0) { ifrotateff = false;root = curr; }
+        node &currn = t[k], &fan = t[fa], &fafan = t[fafa];
+        // for (int i = 0; i < 2; ++i) if(fan.son[i] && meth == i ^ 1) return -1; // ´íÎó´¦Àí
+        int meth = (fan.son[0] != curr);
+        fan.son[meth] = currn.son[meth ^ 1], t[currn.son[meth ^ 1]].father = fa;
+        currn.son[meth ^ 1] = fa, fan.father = curr;
+        if(ifrotateff) {
+        currn.father = fafa;
+        if(fafan.son[meth] == fa) fafan.son[meth] = curr;
+        if(fafan.son[meth ^ 1] == fa) fafan.son[meth ^ 1] = curr;
+        } 
+        pushup(fa); pushup(curr); if(ifrotateff) pushup(fafa);
+    }
+    inline void splay(int s, int e) {
+        // ´Óindex s µ½ e
+        int fa, fafa;
+        while(s != e) {
+            
+        }
     }
 }
 namespace DEBUG {
     using namespace BST;
+    const int cntm = 1e5;
     inline void printOut() {
+        int cnt = 0;
         print(root, true);
         queue<BST::node> q;
         q.push(t[root]);
         cout << "root: "<< t[root].val << endl;
         while(!q.empty()) {
+            if(cnt > cntm) break;
+            ++cnt;
             auto curr = q.front();
             q.pop();
             cout << curr.val << "'s son: ";
@@ -139,7 +174,7 @@ namespace DEBUG {
         }
         p.clear();
     }
-    }
+}
 int main() {
     srand(time(NULL));
     using namespace DEBUG;
@@ -150,21 +185,49 @@ int main() {
         cin >> k;
         insert(root, k);
     }
+    // printOut();
     /*
     printOut();
-    cout << endl;
     rotate(find(root, 4));
-    p.clear();
-    print(root, false);
+    printOut();
+    rotate(find(root, 4));
+    printOut();
+    rotate(find(root, 4));
     printOut();
     */
+    rotate(find(root, 4));
+    rotate(find(root, 4));
+    rotate(find(root, 4));
     printOut();
-    rotate(find(root, 4), 0);
+    print(root, false);
+    cout << find(root, 4) << endl;
+    /*
+    for (int i = 0; i < cnt; ++i) {
+        if(t[i].val == 4) cout << i << endl;
+    }
+    cout << "t[i].val => " <<t[5].val << endl;
+    cout << "t[i].size => " << t[5].size << endl;
+    cout << "t[i].father => " << t[5].father << endl;
+    cout << "t[i].son[0] => " << t[5].son[0] <<endl;
+    cout << "t[i].son[1] => " << t[5].son[1] << endl;
+    cout << find(root, 4) <<endl; // »¹ÊÇÕÒ²»µ½ ÄªÃûÆäÃî¾Í²»¼ûÁËqwq...
     cout << endl;
     printOut();
     print(root, false);
+    */
 }
 /*
 6
 2 8 9 6 4 1
+*/
+/*
+9
+1 6 7 4 3 8 9 2 5
+*/
+/*
+9 
+1 2 3 4 5 6 7 8 9
+*/
+/*
+±¾´Îµ÷ÊÔÎªÁË±£Ö¤¸ù½ÚµãÄÜ¹»Ðý×ª
 */
