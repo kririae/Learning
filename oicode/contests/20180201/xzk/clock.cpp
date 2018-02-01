a8bc2d9bc5d72afb4822b91995222a1544883674
kririae <kririae@yuyuko.cc>
2018-02-01 16:31:15 +0800

    update

--- oicode/contests/20180201/xzk/clock.cpp
+++ oicode/contests/20180201/xzk/clock.cpp
@@ -5,28 +5,22 @@ inline bool is_lucky_number(int t, int b)
 	return t % 10 == 7 || (t / 10) % 10 == 7 || b % 10 == 7 || (b / 10) % 10 == 7;
 }
 int main() {
-	freopen("clock.in", "r", stdin);
-	freopen("clock.out", "w", stdout);
+	// freopen("clock.in", "r", stdin);
+	// freopen("clock.out", "w", stdout);
 	int x, hh, mm;
 	cin >> x >> hh >> mm;
 	int cnt = 0;
 	while(!is_lucky_number(hh, mm))
 	{
-		if(mm > x) {
-			mm -= x;
-		}
-		if(mm <= x) {
+		if(mm - x >= 0) mm -= x;
+		else {
 			mm = 60 - (x - mm);
-			if(hh == 0) {
-				hh = 23;
-			}
-			else {
-				--hh;
-			}
+			if(hh - 1 >= 0) --hh;
+			else hh = 23;
 		}
 		++cnt;
 		// cout << hh << " " << mm << endl;
 	}
 	cout << cnt << endl;
-	fclose(stdin); fclose(stdout);
+	// fclose(stdin); fclose(stdout);
 }
