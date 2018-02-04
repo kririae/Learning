eb87fcfa1fd017ec45ab107f9c59012bbce0abeb
kriaeth <kriaeth@yuyuko.cc>
2018-02-04 22:35:25 +0800

    一些小的修改和photos

--- oicode/template/sprasetable.cpp
+++ oicode/template/sprasetable.cpp
@@ -7,3 +7,15 @@ inline void st_init() {
 		for (int i = 1; i <= (n - (1 << j) +1); ++i) 
 			f[i][j] = max(f[i][j - 1], f[i + (1 << (j - i))][j - 1]);
 }
+
+int n;
+int logm = log(n) / log(2) + 1;
+const int maxn = 1e5;
+int a[maxn];
+int f[maxn][logm];
+inline void st_init() {
+	for (int i = 1; i <= n; ++i) f[i][0] = a[i];
+	for (int j = 1; j < logm; ++j) {
+		for (int i = 1; i <= ())
+	}
+}
