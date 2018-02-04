027fb69c4ab0f8c46e4f4b5bdbfb492413fbdbc3
kririae <kririae@yuyuko.cc>
2018-02-05 00:38:56 +0800

    下载到了一些明天可能会用到的学长的资料

---
+++ oicode/resource/Qizy/陈立杰的遗产/BZOJ全代码_files/cnzz_core.php
@@ -0,0 +1,9 @@
+(function(){
+try{
+var d=window._CNZZDbridge_2982771.bobject;
+var scheme =  (document.location.protocol=='https:')?'https:':'http:';
+d.callRequest([]);
+d.callScript([]);
+        d.createIcon(["<a href='http://www.cnzz.com/stat/website.php?web_id=2982771' target=_blank title='&#31449;&#38271;&#32479;&#35745;'>&#31449;&#38271;&#32479;&#35745;</a>"])}catch(err){
+}
+})();
