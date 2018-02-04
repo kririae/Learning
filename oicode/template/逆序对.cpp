05801926ef17ada7a33251195f0a21d2906febbf
kririae <kririae@yuyuko.cc>
2018-02-04 09:40:19 +0800

    倍增问题

--- oicode/template/逆序对.cpp
+++ oicode/template/逆序对.cpp
@@ -56,59 +56,77 @@ int main() {
 using std::cout;
 using std::cin;
 using std::endl;
+using std::pair;
+
 namespace solve {
-    template<typename T>
-    auto min = [](T a, T b) { return a < b ? a : b; };
+template<typename T>
+auto min = [](T a, T b) { return a < b ? a : b; };
 
-    template<typename T>
-    auto max = [](T a, T b) { return a < b ? b : a; };
+template<typename T>
+auto max = [](T a, T b) { return a < b ? b : a; };
 
-    inline char read() {
-        static const int IN_LEN = 1000000;
-        static char buf[IN_LEN], *s, *t;
-        s == t ? t = (s = buf) + fread(buf, 1, IN_LEN, stdin) : 0;
-        return s == t ? -1 : *s++;
+inline char read() {
+    static const int IN_LEN = 1000000;
+    static char buf[IN_LEN], *s, *t;
+    s == t ? t = (s = buf) + fread(buf, 1, IN_LEN, stdin) : 0;
+    return s == t ? -1 : *s++;
+}
+
+template<typename T>
+inline void read(T &x) {
+    static char c;
+    static bool iosig;
+    for (c = read(), iosig = false; !isdigit(c); c = read()) {
+        if (c == -1) return;
+        c == '-' ? iosig = true : 0;
     }
+    for (x = 0; isdigit(c); c = read()) x = (x + (x << 2) << 1) + (c ^ '0');
+    iosig ? x = -x : 0;
+}
 
-    template<typename T>
-    inline void read(T &x) {
-        static char c;
-        static bool iosig;
-        for (c = read(), iosig = false; !isdigit(c); c = read()) {
-            if (c == -1) return;
-            c == '-' ? iosig = true : 0;
-        }
-        for (x = 0; isdigit(c); c = read()) x = (x + (x << 2) << 1) + (c ^ '0');
-        iosig ? x = -x : 0;
-    }
+const int maxn = 40005;
+const int inf = ~(1 << 31);
+int n, m;
+pair<int, int> a[maxn]; // val, posi
+int cnt = 0; // 算了这个省选题就明天来...
+int c[maxn], dct[maxn];
+/*
+ * 思路如下
+ * 正常加入，但是既然要统计数据
+ * 那么，有一个思路，每一位的和为i，然后统计前缀和就好了嘛qwq
+ * 倒着走，然后输出
+ * 祈祷一发明天不会考试
+ * dct: 离散化数组
+ * i:  1 2 3 4 5 -> v
+ * v:  8 5 8 1 2 
+ * 排序后
+ * i:  4 5 2 1 3 
+ * v:  1 2 5 8 8
+ * dct:4 3 5 1 2 -> i
+ * 离散化i
+ */
+auto lowbit = [](int t) -> int { return t & -t; };
+auto insert = [](int t, int val) { for(int i = t; i <= n; i += lowbit(i)) c[i] += val; };
+auto getsum = [](int t) -> long long { 
+    long long res = 0; 
+    for (int i = t; i >= 0; i -= lowbit(i)) 
+        res += c[i]; 
+    return res;
+}
+auto erase = [](int t)
+inline void init() {
+    read(n);
+    read(m);
+    for (int i = 1; i <= n; ++i) read(a[i].first), a[i].second = i;
+    sort(a + 1, a + n + 1);
+    for (int i = 1; i <= n; ++i) dct[a[i].second] = i;
+}
 
-    const int maxn = 40005;
-    const int inf = ~(1 << 31);
-    int n, m;
-    pair<int, int> a[maxn]; // val, posi
-    int cnt = 0; // 算了这个省选题就明天来...
-    /*
-     * 思路如下
-     * 正常加入，但是既然要统计数据
-     * 那么，有一个思路，每一位的和为i，然后统计前缀和就好了嘛qwq
-     * 既然是删除了不会加入，那和星球大战一样，倒着加就对了，其他的都设置成inf
-     * 倒着走，然后输出
-     * 祈祷一发明天不会考试
-     * 明早写，如果今晚写的话别想睡觉了...晚安w
-     */
-    auto lowbit = [](int t) { return t & -t; };
-    auto insert = [](int t) {};
-
-    inline void init() {
-        read(n);
-        read(m);
-        for (int i = 1; i <= n; ++i) read(a[i].first), a[i].second = i;
-        sort(a + 1, a + n + 1);
-    }
-
-    inline void solve() {
-
-    }
+inline void solve() {
+    long long ans = 0;
+    for (int i = 1; i <= n; ++i) insert(dct[i], 1), ans += i - getsum(dct[i]);
+    cout << ans << endl;
+}   
 }
 
 int main() {
