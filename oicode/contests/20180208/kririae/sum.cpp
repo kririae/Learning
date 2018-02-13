d0e18291672e626b7fdae39cceebac35124cb581
kriaeth <kriaeth@outlook.com>
2018-02-13 21:55:26 +0800

    qwq

--- oicode/contests/20180208/kririae/sum.cpp
+++ oicode/contests/20180208/kririae/sum.cpp
@@ -1,22 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<typename T>
-inline void read(T &val) {
-	val = 0; int f = 1;
+template <typename T>
+inline void read(T &val)
+{
+	val = 0;
+	int f = 1;
 	char ch = getchar();
-	while(!isdigit(ch)) {
-		if(ch == '-') f = -1;
+	while (!isdigit(ch))
+	{
+		if (ch == '-')
+			f = -1;
 		ch = getchar();
 	}
-	while(isdigit(ch)) {
+	while (isdigit(ch))
+	{
 		val = val * 10 + ch - '0';
 		ch = getchar();
 	}
 	val *= f;
 }
 
-inline int gcd(int a, int b) {
+inline int gcd(int a, int b)
+{
 	return !b ? a : gcd(b, a % b);
 }
 
@@ -24,23 +30,25 @@ const int maxn = 5000005;
 const int mod = 1e9 + 7;
 int n;
 int a[maxn];
-int main() 
+int main()
 {
 	freopen("sum.in", "r", stdin);
 	freopen("sum.out", "w", stdout);
 	read(n);
-	for (int i = 1; i <= n; ++i) 
+	for (int i = 1; i <= n; ++i)
 		read(a[i]);
 	long long ans = 0;
 	register int i = 1;
 	register int j = 1;
 	long long res;
-	for (i = 1; i <= n; ++i) {
-		for (j = 1; j <= n; ++j) {
+	for (i = 1; i <= n; ++i)
+	{
+		for (j = 1; j <= n; ++j)
+		{
 			res = gcd(a[i], a[j]);
 			// printf("gcd(%d, %d) = %d\n", a[i], a[j], res);
 			ans += (long long)(res * (res - 1)) % mod;
 		}
-	}	
-	cout << ans % mod<< endl;
+	}
+	cout << ans % mod << endl;
 }
--- oicode/contests/20180208/std/sum.cpp
+++ oicode/contests/20180208/std/sum.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define LL long long
 using namespace std;
 
@@ -7,33 +7,43 @@ const int MOD = 1000000007;
 
 int f[N];
 
-inline int read() {
-    char c = getchar(); int ret = 0, f = 1;
-    for (; c < '0' || c > '9'; f = c == '-'? -1: 1, c = getchar());
-    for (; '0' <= c && c <= '9'; ret = ret * 10 + c - '0', c = getchar());
+inline int read()
+{
+    char c = getchar();
+    int ret = 0, f = 1;
+    for (; c < '0' || c > '9'; f = c == '-' ? -1 : 1, c = getchar())
+        ;
+    for (; '0' <= c && c <= '9'; ret = ret * 10 + c - '0', c = getchar())
+        ;
     return ret * f;
 }
 
-int main() {
+int main()
+{
     freopen("sum.in", "r", stdin);
     freopen("sum.out", "w", stdout);
     int n = read(), mx = 0, ans = 0;
-    for (int i = 1, w; i <= n; i++) {
+    for (int i = 1, w; i <= n; i++)
+    {
         f[w = read()]++;
         mx = max(mx, w);
     }
-    for (int i = 1; i <= mx; i++) {
-        for (int j = i + i; j <= mx; j += i) {
+    for (int i = 1; i <= mx; i++)
+    {
+        for (int j = i + i; j <= mx; j += i)
+        {
             f[i] += f[j];
         }
     }
-    for (int i = mx; i; i--) {
+    for (int i = mx; i; i--)
+    {
         f[i] = (LL)f[i] * f[i] % MOD;
-        for (int j = i + i; j <= mx; j += i) {
+        for (int j = i + i; j <= mx; j += i)
+        {
             (f[i] -= f[j]) %= MOD;
         }
         ans = ((LL)i * (i - 1) * f[i] + ans) % MOD;
     }
-    cout<<(ans + MOD) % MOD<<endl;
+    cout << (ans + MOD) % MOD << endl;
     return 0;
 }
