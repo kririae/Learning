8093593a5bc9b46fe78c505194a3a851297c3b4f
kriaeth <kriaeth@outlook.com>
2018-02-21 16:35:49 +0800

    delete

--- oicode/contests/codeforces/codeforces round #462/a.cpp
+++ oicode/contests/codeforces/codeforces round #462/a.cpp
@@ -1,26 +1,6 @@
 #include <bits/stdc++.h>
 namespace qwq {
 
-inline char read() {
-  static const int IN_LEN = 1000000;
-  static char buf[IN_LEN], *s, *t;
-  s == t ? t = (s = buf) + fread(buf, 1, IN_LEN, stdin) : 0;
-  return s == t ? -1 : *s++;
-}
-
-template <typename T> inline void read(T &x) {
-  static char c;
-  static bool iosig;
-  for (c = read(), iosig = false; !isdigit(c); c = read()) {
-    if (c == -1)
-      return;
-    c == '-' ? iosig = true : 0;
-  }
-  for (x = 0; isdigit(c); c = read())
-    x = (x + (x << 2) << 1) + (c ^ '0');
-  iosig ? x = -x : 0;
-}
-
 const int maxn = 55;
 const long long inf = (long long)1e18;
 const long long minf = (long long)-1e18;
--- oicode/template/template.cpp
+++ oicode/template/template.cpp
@@ -1,60 +1,30 @@
 /*
-* by kriaeth
-* time:
-* using template v0.0
-* read function by xehoth
-*/
+ * by kriaeth
+ * time:
+ * using template v0.0
+ * read function by xehoth
+ */
 
-#include <iostream>
-#include <cstdio>
-#include <vector>
 #include <bitset>
+#include <cstdio>
 #include <deque>
+#include <iostream>
 #include <queue>
+#include <vector>
 
-namespace solve
-{
 
-template<typename T>
-auto min = [](T a, T b) { return a < b ? a : b; };
+namespace solve {
 
-template<typename T>
-auto max = [](T a, T b) { return a < b ? b : a; };
+template <typename T> auto min = [](T a, T b) { return a < b ? a : b; };
 
-inline char read()
-{
-	static const int IN_LEN = 1000000;
-	static char buf[IN_LEN], *s, *t;
-	s == t ? t = (s = buf) + fread(buf, 1, IN_LEN, stdin) : 0;
-	return s == t ? -1 : *s++;
-}
+template <typename T> auto max = [](T a, T b) { return a < b ? b : a; };
 
-template <typename T>
-inline void read(T &x)
-{
-	static char c;
-	static bool iosig;
-	for (c = read(), iosig = false; !isdigit(c); c = read()) {
-		if (c == -1) return;
-		c == '-' ? iosig = true : 0;
-	}
-	for (x = 0; isdigit(c); c = read()) x = (x + (x << 2) << 1) + (c ^ '0');
-	iosig ? x = -x : 0;
-}
+inline void init() {}
 
-inline void init()
-{
+inline void solve() {}
+} // namespace solve
 
-}
-
-inline void solve()
-{
-
-}
-}
-
-int main()
-{
-	solve::solve();
-	return 0;
+int main() {
+  solve::solve();
+  return 0;
 }
