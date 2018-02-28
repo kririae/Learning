11f3eb442c276d40954de73d01b45bddd59315fe
kririae <kririae@yuyuko.cc>
2018-02-28 22:49:43 +0800

    qwq

--- oicode/网络流24题/软件补丁问题.cpp
+++ oicode/网络流24题/软件补丁问题.cpp
@@ -1,3 +1,5 @@
+// by kririae
+// 2018/2/28
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn = 105;
@@ -17,26 +19,28 @@ queue<int> q;
 
 inline int spfa()
 {
-	memset(dis, 127, sizeof(dis));
+	for (int i = 0; i < (1 << n); ++i)
+		dis[i] = 1e9;
 	int s = (1 << n) - 1;
 	q.push(s);
 	vis[s] = true;
 	dis[s] = 0;
 
-	while(!q.empty())
+	while (!q.empty())
 	{
-		int curr = q.front(); q.pop(); vis[curr] = false;
+		int curr = q.front();
+		q.pop();
+		vis[curr] = false;
 		for (int i = 0; i < m; ++i)
-		    // 当且仅当软件包含b1的所有错误 且不包含b2的任何一个错误
-			if((t[i].b1 & curr) == t[i].b1 && (t[i].b2 & curr) == 0) // 是否能够加入边
+			// 当且仅当软件包含b1的所有错误 且不包含b2的任何一个错误
+			if ((t[i].b1 & curr) == t[i].b1 && (t[i].b2 & curr) == 0) // 是否能够加入边
 			{
 				int to = curr & (~t[i].f1);
-				to |= t[i].f2;// 修复f1中的错误
-				cout << curr << " " << to << endl;
-				if(dis[to] > dis[curr] + t[i].dis)
+				to |= t[i].f2; // 修复f1中的错误
+				if (dis[to] > dis[curr] + t[i].dis)
 				{
 					dis[to] = dis[curr] + t[i].dis;
-					if(!vis[to])
+					if (!vis[to])
 						vis[to] = true, q.push(to);
 				}
 			}
@@ -56,20 +60,21 @@ int main()
 		cin >> t[i].dis >> pro1 >> pro2;
 		for (int j = 0; j < n; ++j)
 		{
-			if(pro1[j] == '+') t[i].b1 |= k;
-			if(pro1[j] == '-') t[i].b2 |= k;
-			k <<= 1;
-		}
-		k = 1;
-		for (int j = 0; j < n; ++j)
-		{
-			if(pro2[j] == '+') t[i].f1 |= k;
-			if(pro2[j] == '-') t[i].f2 |= k;
+			if (pro1[j] == '+')
+				t[i].b1 |= k;
+			if (pro1[j] == '-')
+				t[i].b2 |= k;
+			if (pro2[j] == '-')
+				t[i].f1 |= k;
+			if (pro2[j] == '+')
+				t[i].f2 |= k;
 			k <<= 1;
 		}
 	}
 	int res = spfa();
-	if(res == 2139062143) cout << -1 << endl;
-	else cout << res << endl;
+	if (res == 1e9)
+		cout << 0 << endl;
+	else
+		cout << res << endl;
 	return 0;
 }
