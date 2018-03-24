d90bc48207e1dba0f3a6ab81aeb03655eb14ce96
kririae <kririae@yuyuko.cc>
2018-03-24 13:34:22 +0800

    poj3635

--- oicode/poj/poj3635.cpp
+++ oicode/poj/poj3635.cpp
@@ -1,6 +1,14 @@
+// STATUS: WA
 // FULL TANK POJ3635
 // 暴力 + DP
-#include < bits/stdc++.h > 
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <bitset>
+
 using namespace std; 
 
 namespace IO
@@ -9,8 +17,7 @@ inline char read()
 {
     static const int IN_LEN = 1000000;
     static char buf[IN_LEN], *s, *t;
-    s == t ? t = (s = buf) +
-                    fread(buf, 1, IN_LEN, stdin) : 0;
+    s == t ? t = (s = buf) + fread(buf, 1, IN_LEN, stdin) : 0;
     return s == t ? -1 : *s++;
 }
 
@@ -19,15 +26,12 @@ inline void read(T &x)
 {
     static char c;
     static bool iosig;
-    for (c = read(), iosig = false;
-            !isdigit(c); c = read())
+    for (c = read(), iosig = false; !isdigit(c); c = read())
     {
         if (c == -1) return;
         c == '-' ? iosig = true : 0;
     }
-    for (x = 0; isdigit(c);
-            c = read())
-        x = (x + (x << 2) << 1) + (c ^ '0');
+    for (x = 0; isdigit(c); c = read()) x = (x + (x << 2) << 1) + (c ^ '0');
     iosig ? x = -x : 0;
 }
 }
@@ -38,22 +42,22 @@ struct Edge
 {
     int from, to, val;
     Edge() {}
-    Edge(int f, int t, int v): from(f), to(t), val(v)
-    {}
+    Edge(int f, int t, int v): from(f), to(t), val(v) {}
 };
 
 struct status
 {
-    int city, cost, oil;
+    int city, cost, oil; // 当前点在city，花费了cost，剩余油量是oil
+    status() {}
+    status(int c, int co, int oi): city(c), cost(co), oil(oi) {}
     bool operator < (const status (&a)) const 
-    {
-        return cost > a.cost;
-    }
+    { return cost > a.cost; }
 };
 
 const int maxn = 1005;
 int n, m, p[maxn], u, v, d, b[maxn][maxn], q, c, s, e;
 vector<Edge> edges[maxn];
+// bool vis[maxn][maxn];
 bitset<maxn> vis[maxn];
 
 inline void addedge(int from, int to, int val)
@@ -62,18 +66,41 @@ inline void addedge(int from, int to, int val)
     edges[to].push_back(Edge(to, from, val));
 }
 
-inline int solve(int c, int s, int e)
+inline void solve(int c, int s, int e)
 {
     priority_queue<status> pq;
     memset(b, 0x3f, sizeof(b));
-    for (int i = 1; i <= n; ++i) vis[i].reset(0);
+    // memset(vis, 0, sizeof(vis));
+    for (int i = 0; i <= n; ++i) vis[i].reset();
     b[s][0] = 0;
-
+    
+    pq.push(status(s, 0, 0));
+    
     while(!pq.empty())
     {
-        statuc curr = pq.top(); pq.pop();
+        status curr = pq.top(); pq.pop();
+        // cout << curr.city << " " << curr.oil << endl;
+        if(vis[curr.city][curr.oil]) continue;
+        vis[curr.city][curr.oil] = 1;
+
+        if(curr.city == e) { cout << curr.cost << endl; return; }
+        if(curr.oil < c && !vis[curr.city][curr.oil + 1] && b[curr.city][curr.oil] + p[curr.city] < b[curr.city][curr.oil + 1])
+        {
+            pq.push(status(curr.city, curr.cost + p[curr.city], curr.oil + 1));
+            b[curr.city][curr.oil + 1] = b[curr.city][curr.oil] + p[curr.city];
+        }
+
+        for (int i = 0; i < edges[curr.city].size(); ++i)
+        {
+            Edge &e = edges[curr.city][i];
+            if(curr.oil >= e.val && !vis[e.to][curr.oil - e.val] && b[e.to][curr.oil - e.val] > curr.cost)
+            {
+                b[e.to][curr.oil - e.val] = curr.cost;
+                pq.push(status(e.to, curr.cost, curr.oil - e.val));
+            }
+        }
     }
-    return -1;
+    cout << "impossible" << endl;
 }
 
 inline void solve()
@@ -93,8 +120,8 @@ inline void solve()
         read(c); read(s); read(e);
         ++s, ++e;
         // b[city][fuel] -> 城市编号，剩余油量
-        int res = solve(c, s, e);
-        cout << res == -1 ? "impossible" : res << endl;
+        solve(c, s, e);
+        // cout << (res == -1 ? "impossible" : res) << endl;
     }
 }
 }
