2d3ff469a4c9e689c7383bc2b2966c409e8d596c
kririae <kririae@yuyuko.cc>
2018-04-20 22:02:16 +0800

    orz

--- oicode/kririae/线段树/luogu3373.cpp
+++ oicode/kririae/线段树/luogu3373.cpp
@@ -4,137 +4,189 @@
 
 using namespace std;
 
-namespace SegmentTree
+namespace IO
 {
-const int maxn = 1e6 + 5;
+inline char read()
+{
+	static const int IN_LEN = 1000000;
+	static char buf[IN_LEN], *s, *t;
+	s == t ? t = (s = buf) + fread(buf, 1, IN_LEN, stdin) : 0;
+	return s == t ? -1 : *s++;
+}
+
+template <typename T>
+inline void read(T &x)
+{
+	static char c;
+	static bool iosig;
+	for (c = read(), iosig = false; !isdigit(c); c = read())
+	{
+		if (c == -1) return;
+		c == '-' ? iosig = true : 0;
+	}
+	for (x = 0; isdigit(c); c = read()) 
+		x = (x + (x << 2) << 1) + (c ^ '0');
+	iosig ? x = -x : 0;
+}
+}
+
+namespace sgt2
+{
+const int maxn = 100005;    
+
 struct Node
 {
-	int l, r, add, mul, son[2];
-	long long sum;
-	Node() {}
+	int l, r, son[2];
+	long long sum, add, mul;
 };
 
 Node t[maxn << 2];
-int cnt, p, a[maxn];
+int cnt;
+long long p;
+
+template<typename T>
+inline void acc(T &a, const T &b)
+{
+	a = (a + b) % p;
+}
+
+template<typename T>
+inline void mut(T &a, const T &b)
+{
+	a = (a * b) % p;
+}
 
 inline void pushup(int k)
 {
-	t[k].sum = (t[ls].sum + t[rs].sum) % p;
+	t[k].sum = t[ls].sum + t[rs].sum;
 }
 
 inline void pushdown(int k)
 {
 	if(t[k].mul != 1)
 	{
-		t[ls].add = (t[ls].add * t[k].mul) % p;
-		t[rs].add = (t[rs].add * t[k].mul) % p;
-		t[ls].mul = (t[ls].mul * t[k].mul) % p;
-		t[rs].mul = (t[rs].mul * t[k].mul) % p;
-		t[ls].sum = (t[ls].sum * t[k].mul) % p;
-		t[rs].sum = (t[rs].sum * t[k].mul) % p;
+		mut(t[ls].add, t[k].mul);
+		mut(t[rs].add, t[k].mul);
+		mut(t[ls].mul, t[k].mul);
+		mut(t[rs].mul, t[k].mul);
+		mut(t[ls].sum, t[k].mul);
+		mut(t[rs].sum, t[k].mul);
 		t[k].mul = 1;
 	}
+	
 	if(t[k].add)
 	{
-		t[ls].add = (t[ls].add + t[k].add) % p;
-		t[rs].add = (t[rs].add + t[k].add) % p;
-		t[ls].sum = (t[ls].sum + t[k].add * (t[ls].r - t[ls].l + 1)) % p;
-		t[rs].sum = (t[rs].sum + t[k].add * (t[rs].r - t[rs].l + 1)) % p;
+		acc(t[ls].add, t[k].add);
+		acc(t[rs].add, t[k].add);
+		acc(t[rs].sum, t[k].add * (t[rs].r - t[rs].l + 1));
+		acc(t[ls].sum, t[k].add * (t[ls].r - t[ls].l + 1));
 		t[k].add = 0;
 	}
 }
 
-inline void buildTree(int &k, int l, int r)
+inline void buildTree(int &k, int l, int r, int *x)
 {
 	k = ++cnt;
-	t[k].l = l, t[k].r = r, t[k].sum = 0, t[k].add = 0, t[k].mul = 1;
-	if(l == r) { t[k].sum = a[l]; return; }
-	int mid = (l + r) >> 1;
-	buildTree(ls, l, mid), buildTree(rs, mid + 1, r);
-	pushup(k);
+	t[k].l = l, t[k].r = r, t[k].mul = 1;
+
+	if(l == r)
+	{
+	    t[k].sum = x[l];
+	} else {
+    	int mid = (l + r) >> 1;
+    	buildTree(ls, l, mid, x);
+    	buildTree(rs, mid + 1, r, x);
+    
+    	pushup(k);
+    }
 }
 
-inline void mul(int k, int l, int r, int val)
+inline void add(int k, int l, int r, long long val)
 {
+    pushdown(k);
+    
 	if(t[k].l == l && t[k].r == r)
 	{
-		t[k].mul = (t[k].mul * val) % p;
-		t[k].add = (t[k].add * val) % p;
-		t[k].sum = (t[k].sum * val) % p;
+		acc(t[k].add, val);
+		acc(t[k].sum, val * (r - l + 1));
 		return;
 	}
-	
-	int mid = (t[k].l + t[k].r) >> 1;
-	pushdown(k);
-	
-	if(r <= mid) mul(ls, l, r, val);
-	else if(l >= mid + 1) mul(rs, l, r, val);
-	else mul(ls, l, mid, val), mul(rs, mid + 1, r, val);
 
-	pushup(k);
-}
-
-inline void add(int k, int l, int r, int val)
-{
-	if(t[k].l == l && t[k].r == r)
-	{
-		t[k].add = (t[k].add + val) % p;
-		t[k].sum = (t[k].sum + val * (r - l + 1)) % p;
-		return;
-	}
 	int mid = (t[k].l + t[k].r) >> 1;
-	pushdown(k);
-	
 	if(r <= mid) add(ls, l, r, val);
 	else if(l >= mid + 1) add(rs, l, r, val);
 	else add(ls, l, mid, val), add(rs, mid + 1, r, val);
-	
+
+	pushup(k);
+}
+
+inline void mul(int k, int l, int r, long long val)
+{
+	if(t[k].l == l && t[k].r == r)
+	{
+		mut(t[k].add, val), mut(t[k].mul, val);
+		mut(t[k].sum, val);
+		return;
+	}
+
+	pushdown(k);
+
+	int mid = (t[k].l + t[k].r) >> 1;
+	if(r <= mid) mul(ls, l, r, val);
+	else if(l >= mid + 1) mul(rs, l, r, val);
+	else mul(ls, l, mid, val), mul(rs, mid + 1, r, val);	
+
 	pushup(k);
 }
 
 inline long long query(int k, int l, int r)
 {
-	pushdown(k);
-	
 	if(t[k].l == l && t[k].r == r)
-		return t[k].sum % p;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-	
+		return t[k].sum;
+    
+    pushdown(k);
+    
 	int mid = (t[k].l + t[k].r) >> 1;
-	if(r <= mid) return query(ls, l, r);
-	else if(l >= mid + 1) return query(rs, l, r);
+	if(r <= mid) return query(ls, l, r) % p;
+	else if(l >= mid + 1) return query(rs, l, r) % p;
 	else return (query(ls, l, mid) + query(rs, mid + 1, r)) % p;
 }
 
-int n, m, root;
+int n, m, a[maxn], root;
+int o, x, y, k;
+
 inline void solve()
 {
-	cin >> n >> m >> p;
-	for (int i = 1; i <= n; ++i) cin >> a[i];
-	
-	buildTree(root, 1, n);
-	
-	for (int i = 1; i <= m; ++i)
-	{
-		int op, x, y, z;
-		cin >> op;
-		if(op == 1)
-		{
-			cin >> x >> y >> z;
-			mul(root, x, y, z);
-		}
-		
-		if(op == 2)
-		{
-			cin >> x >> y >> z;
-			add(root, x, y, z);
-		}
-		
-		if(op == 3)
-		{
-			cin >> x >> y;
-			cout << query(root, x, y) << endl;
-		}
-	}
+	using namespace IO;
+	read(n); read(m); read(p);
+    
+    for (int i = 1; i <= n; ++i)
+        read(a[i]);
+        
+    buildTree(root, 1, n, a);
+    
+    for (int i = 1; i <= m; ++i)
+    {
+    	read(o);
+
+    	if(o == 1)
+    	{
+    		read(x); read(y); read(k);
+    		mul(root, x, y, k);
+    	}
+
+    	if(o == 2)
+    	{
+    		read(x); read(y); read(k);
+    		add(root, x, y, k);
+    	}
+
+    	if(o == 3)
+    	{
+    		read(x); read(y);
+    		cout << query(root, x, y) << endl;
+    	}
+    }
 }
 }
 
@@ -142,6 +194,7 @@ int main()
 {
 	cin.tie(0);
 	ios::sync_with_stdio(false);
-	SegmentTree::solve();
+
+	sgt2::solve();
 	return 0;
 }
