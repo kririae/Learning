e15dd9d327379a882c326f7a7260ee2643e067c9
kririae <kririae@yuyuko.cc>
2018-04-29 23:58:22 +0800

    qwq

--- oicode/kririae/数据结构/线段树/NOIP2017D2T3_TODO.cpp
+++ oicode/kririae/数据结构/线段树/NOIP2017D2T3_TODO.cpp
@@ -1,103 +1,108 @@
-// by kririae
-// NOIP2017 D2T3
-// SegmentTree ver
-// TODO~
-#define ls t[k].son[0]
-#define rs t[k].son[1]
+#define ls t[root][k].son[0]
+#define rs t[root][k].son[1]
 #include <bits/stdc++.h>
 
 using namespace std;
 
-namespace IO
-{
-inline char read()
-{
-	static const int len = 1000000;
-	static char buf[len], *s, *t;
-	s == t ? t = (s = buf) + fread(buf, 1, len, stdin) : 0;
-	return s == t ? -1 : *s++;
-}
-
-template <typename T>
-inline void read(T &x)
-{
-	static char c;
-	static bool minus;
-	for (c = read(), minus = false; !isdigit(c); c = read())
-	{
-		if (c == -1) return;
-		c == '-' ? minus = true : 0;
-	}
-	for (x = 0; isdigit(c); c = read()) 
-		x = (x + (x << 2) << 1) + (c ^ '0');
-	minus ? x = -x : 0;
-}
-}
-
-namespace Sylvia
+namespace NOIP2017D2T3
 {
 const int maxn = 300005;
 
 struct Node
 {
-	int l, r, son[2];
-	long long sum, add;
+	int l, r, son[2];    
+	long long sum, add, set;
+
+	Node() : set(-1) {}
+	Node(int li, int ri) : l(li), r(ri) {}
+
+	void modify_add(int val)
+	{
+		add += val;
+		sum += (r - l + 1) * val;
+	}
+
+	void modify_set(int val)
+	{
+		set = val, add = 0;
+		sum = (r - l + 1) * val;
+	}
 };
 
 vector<Node> t[maxn];
-int n, m, q, x, y;
-// 第0棵不存在qwq
 
-inline long long summ(int line, int l, int r)
+inline void pushdown(int root, int k)
 {
-	return ((2 + (m * line) << 1 + l + r - 2) * (r - l + 1)) >> 1;
+	if(!ls)
+	{
+		t[root].push_back(Node(t[root][k].l, (t[root][k].l + t[root][k].r) >> 1));
+		ls = t[root].size() - 1;
+	}
+
+	if(!rs)
+	{ 
+		t[root].push_back(Node((t[root][k].l + t[root][k].r) >> 1) + 1, t[root][k].r);
+		rs = t[root].size() - 1;
+	}
+
+	if(t[root][k].set != -1)
+	{
+		t[root][ls].modify_set(t[root][k].set), t[root][rs].modify_set(t[root][k].set);
+		t[root][k].set = -1;
+	}
+
+	if(t[root][k].add)
+	{
+		t[root][ls].modify_add(t[root][k].add), t[root][rs].modify_add(t[root][k].add);
+		t[root][k].add = 0;
+	}
 }
 
-inline int New(int root)
+inline void pushup(int root, int k)
 {
-	t[root].push_back(Node());
-	return t[root].size() - 1;
+	t[root][k].sum = t[root][ls].sum + t[root][rs].sum;
 }
 
-inline void add(int root, int &k, int l, int r, int val)
+inline void add(int root, int k, int l, int r, int val)
 {
-	k = New(root);
+	if(t[root][k].l == l && t[root][k].r == r)
+	{	
+		t[root][k].modify_add(val);
+		return;
+	}
+
+	pushdown(root, k);
+
+	int mid = (t[root][k].l + t[root][k].r) >> 1;
+	if(r <= mid) add(root, ls, l, r, val);
+	else if (l > mid) add(root, rs, l, r, val);
+	else add(root, ls, l, mid, val), add(root, rs, mid + 1, r, val);
+
+	pushup(root, k);
 }
 
-inline void set(int root, int &k, int pos, int val)
+inline void query(int root, int k, int l, int r, int val)
+{
+	int b = 
+}
+
+inline void proc(int x, int y)
 {
 
 }
 
-inline long long query(int root, int &k, int pos)
-{
-
-}
-
+int n, m, q;
 inline void solve()
 {
-	using namespace IO;
-	read(n); read(m); read(q);
+	cin >> n >> m >> q;　
 
+	t[0].push_back(Node(1, n));
 	for (int i = 1; i <= n; ++i)
-	{
-		t[i].push_back
-		((Node) {1, m, {0, 0}, (2 + 2 * m * i) * m / 2, 0})
-	}
-
-	for (int i = 1; i <= n; ++i)
-	{
-		read(x); read(y);
-
-	}
+		t[i].push_back(Node(1, m - 1));
 }
 }
-
-int main(int argc, char const *argv[])
+int main()
 {
-	cin.tie(0);
-	ios::sync_with_stdio(false);
-
-	Sylvia::solve();
+	NOIP2017D2T3::solve();
 	return 0;
 }
