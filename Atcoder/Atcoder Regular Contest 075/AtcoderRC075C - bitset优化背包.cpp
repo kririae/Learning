9ebb9831eecd98569f72ffe37b73e03e98d2246b
kririae <kririae@yuyuko.cc>
2018-06-23 17:36:08 +0800

    qaq

---
+++ Atcoder/Atcoder Regular Contest 075/AtcoderRC075C - bitset优化背包.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 10005;
+
+int n, val, ans;
+bitset<maxn> f;
+int main()
+{ 
+  scanf("%d", &n);
+  for (R int i = 1; i <= n; ++i) scanf("%d", &val), f |= (f << val);
+  for (R int i = 1; i <= 10000; ++i) if(i % 10 != 0 && f[i]) ans = max(ans, i);
+  printf("%d", ans);
+}
