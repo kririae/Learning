bef278d62a9bd101686e2260e9fc13593bbe6aad
kririae <kririae@yuyuko.cc>
2018-10-11 12:11:12 +0800

    ...这T2

--- 杂/石室中学/石室中学20181011/division.cpp
+++ 杂/石室中学/石室中学20181011/division.cpp
@@ -4,41 +4,32 @@ using namespace std;
 
 int id, t, c, m;
 ll n;
-inline ll fpow(ll a, ll p) {
+inline ll fpow(ll a, ll p, ll mod) {
   ll ans = 1;
   for (; p; p >>= 1) {
-    if (p & 1) ans = (1ll * a * ans) % n;
-    a = (a * a) % n;
+    if (p & 1) ans = (1ll * a * ans) % mod;
+    a = (a * a) % mod;
   }
   return ans;
 }
+inline ll calc(ll n, ll m) {
+  ll cnt = 0;
+  for (int i = 1; i <= n; ++i) {
+    ll t = fpow(i, m, n);
+    if ((t - i + n) % n == 0) ++cnt;
+  }
+  return cnt;
+}
 int main() {
-  freopen("division.in", "r", stdin);
-  freopen("division.out", "w", stdout);
   scanf("%d%d", &id, &t);
   while (t--) {
     scanf("%d%d", &c, &m);
     n = 1;
+    ll ans = 1;
     for (int i = 1, x; i <= c; ++i) {
       scanf("%d", &x);
-      n *= x;
+      (ans *= calc(x, m)) %= 998244353;
     }
-    if (m == 1) {
-      cout << n << endl;
-      continue;
-  	}
-    ll cnt = 0;
-    for (int i = 1; i <= n; ++i) {
-      ll t = fpow(i, m);
-      if ((t - i + n) % n == 0)
-        ++cnt;
-    }
-    cout << cnt % 998244353 << endl;
+    printf("%lld\n", ans);
   }
 }
-/*
-0
-1
-2 5
-743 857
- */
